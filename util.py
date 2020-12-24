cdd58968106364f4c2b52fd5c81cf4a408bfa582
Krishna Mani <krishnamani314@gmail.com>
2020-12-24 12:56:20 -0800

    add data processing

--- util.py
+++ util.py
@@ -30,33 +30,50 @@ def tokenize_jpn(input):
     return nagisa.tagging(input).words
 
 
-def prune_training_data(file, out, vocab):
-    data = open(file, 'r').read().split('。')
-
+def tokenize_and_build_vocab(file, out, vocab_len):
+    data = open(file, 'r').read()
     outfile = open(out, 'a')
 
-    max = 0
-    for i in data:
-        # split = (vocab.w2i.get(x) if vocab.w2i.get(x) is not None
-        #          else '<UNK>' for x in nagisa.tagging(i).words)
-        split = []
-        for j in parser.parse(i).split(' '):
+    words = {}
+
+    tokenized = parser.parse(data)
+    for i in tokenized.split(' '):
+        if i in words:
+            words[i] += 1
+            continue
+        words[i] = 1
+
+    # sort by freq
+    words = dict(sorted(words.items(), key=lambda item: item[1], reverse=True))
+    print('{} unique tokens'.format(len(words)))
+
+    # you can get away with much fewer words
+    words = [x for x in words][0:vocab_len]
+    words.append('<SOS>')
+    words.append('<EOS>')
+    words.append('<UNK>')
+
+    print(words)
+    vocab = Vocab(words)
+
+    out = []
+    lines = data.splitlines()
+    print(repr(lines))
+
+    for i in lines:
+        out2 = []
+        out2.append(vocab.w2i.get('<SOS>'))
+        for j in parser.parse(i).strip('\n').split(' ')[:-1]:
             idx = vocab.w2i.get(j)
-            print('{}: {}'.format(j, idx))
-            split.append(idx if idx is not None else '<UNK>')
+            print(repr('{}: {}'.format(j, idx)))
+            out2.append(idx if idx is not None else vocab.w2i.get('<UNK>'))
+        out2.append(vocab.w2i.get('<EOS>'))
+        out.append(out2)
 
-        if len(split) > max:
-            max = len(split)
-
-        outfile.write('<SOS>{}<EOS>\n'.format(split))
-
-    return max
+    for i in out:
+        outfile.write('{}\n'.format(i))
 
 
 # prune_edict('./edict2-utf8.txt', './vocab.txt')
 # prune_leeds_corpus('./leeds.txt', './vocab_freq.txt', -1)
-
-words = open('./vocab.txt').read().splitlines()
-vocab = Vocab(words)
-
-prune_training_data('./data.txt', './split_data.txt', vocab)
+tokenize_and_build_vocab('./data.txt', './split_data.txt', 200)
